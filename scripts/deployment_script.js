8f29cb5ef56130fb717847b2a87fa0eb935abcef
Jakub <jakubpchmiel@gmail.com>
2021-11-25 06:23:03 +0100

    Integrate hardhat

---
+++ scripts/deployment_script.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  
+  const CraftlinkERC20 = await hre.ethers.getContractFactory("CraftlinkERC20");
+  const craftlinkERC20 = await CraftlinkERC20.deploy();
+
+  await craftlinkERC20.deployed();
+
+  console.log("CraftlinkERC20 deployed to:", craftlinkERC20.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
